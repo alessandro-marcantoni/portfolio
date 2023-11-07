958c435d722d61695de3ad598288e8419a775076
Alessandro Marcantoni <alessandro.marcantoni.98@gmail.com>
2023-11-07 12:51:35 +0100

    chore: generate svelte project

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Portfolio from "./lib/Portfolio.svelte";
+</script>
+
+<main class="bg-dark vh-100">
+  <Portfolio />
+</main>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import App from './App.svelte'
+import './style/bootstrap.css'
+import './style/debug.css'
+import './style/style.css'
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
