2ea0ed16e9cec0f14825c9583f3f14255cd36781
Alessandro Marcantoni <alessandro.marcantoni.98@gmail.com>
2023-11-17 23:12:47 +0100

    chore: changes

---
+++ src/contents/sections.ts
@@ -0,0 +1,10 @@
+export type Section = {
+    title: string
+    name: string
+}
+
+const sections: Section[] = [
+    { title: 'Home', name: 'home' },
+    { title: 'Resume', name: 'resume' },
+    { title: 'Projects', name: 'projects' },
+]
